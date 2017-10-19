af7c467709b2771987260ee40a70c7b7625c0518
OleKjaerulff <okjaerulff@sund.ku.dk>
2017-10-19 18:11:19 +0200

    still working with fields

--- PillPiliotApp/Resources/Resource.Designer.cs
+++ PillPiliotApp/Resources/Resource.Designer.cs
@@ -44,17 +44,26 @@ namespace PillPiliotApp
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f040004
+			public const int linearLayout1 = 2130968580;
+			
 			// aapt resource value: 0x7f040001
-			public const int linearLayout1 = 2130968577;
+			public const int linearLayout2 = 2130968577;
+			
+			// aapt resource value: 0x7f040006
+			public const int morgenDosis1 = 2130968582;
+			
+			// aapt resource value: 0x7f040005
+			public const int morgenNavn1 = 2130968581;
 			
 			// aapt resource value: 0x7f040003
-			public const int morgenDosis1 = 2130968579;
-			
-			// aapt resource value: 0x7f040002
-			public const int morgenNavn1 = 2130968578;
+			public const int titleDOSISmorgen = 2130968579;
 			
 			// aapt resource value: 0x7f040000
 			public const int titleMORGEN = 2130968576;
+			
+			// aapt resource value: 0x7f040002
+			public const int titleNAVNmorgen = 2130968578;
 			
 			static Id()
 			{
